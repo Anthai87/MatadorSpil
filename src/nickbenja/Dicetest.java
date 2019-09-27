26834206dd9b73d37c895f282700795ee50b1c33
anlc92 <s180646@student.dtu.dk>
2019-09-27 11:36:39 +0200

    nu med test og game

--- src/nickbenja/Dicetest.java
+++ src/nickbenja/Dicetest.java
@@ -1,11 +1,9 @@
 package nickbenja;
 
-public class Dice {
+public class Dicetest {
 
 
     public static void main(String[] args) {
-        //int diceX=(int)(Math.random()*6+1); int diceY=(int)(Math.random()*6+1); int sum1=diceX+diceY;
-        //System.out.println("you rolled " + diceX + " + " + diceY +  " = " + sum1);
 
         int count = 1;
         while (count <= 1000) {
---
+++ src/nickbenja/Game.java
@@ -0,0 +1,4 @@
+package nickbenja;
+
+public class Game {
+}
