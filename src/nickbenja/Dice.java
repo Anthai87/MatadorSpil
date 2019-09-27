053a6a599d5ed2ea0c9f4456a598282bb5c60db4
Bamse <bbandresen@hotmail.com>
2019-09-27 11:21:18 +0200

    terning (V) Test (V)

--- src/nickbenja/Dice.java
+++ src/nickbenja/Dice.java
@@ -1,4 +1,18 @@
 package nickbenja;
 
 public class Dice {
+
+
+    public static void main(String[] args) {
+        //int diceX=(int)(Math.random()*6+1); int diceY=(int)(Math.random()*6+1); int sum1=diceX+diceY;
+        //System.out.println("you rolled " + diceX + " + " + diceY +  " = " + sum1);
+
+        int count = 1;
+        while (count <= 1000) {
+            int diceX=(int)(Math.random()*6+1); int diceY=(int)(Math.random()*6+1); int sum1=diceX+diceY;
+            System.out.println("you rolled " + diceX + " + " + diceY +  " = " + sum1 + " amout of rolls " + count);
+            count++;
+        }
+
+    }
 }
