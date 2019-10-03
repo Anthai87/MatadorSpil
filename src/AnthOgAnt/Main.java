e71c3ed9a6e44787480dff63a86457a72986ba2b
antonoeschmidt <s163053@student.dtu.dk>
2019-10-03 10:40:15 +0200

    Ændret småting. Sprog og andre tekstændringer.

--- src/AnthOgAnt/Main.java
+++ src/AnthOgAnt/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
         Player player1 = new Player("Anton", 0, false);
         Player player2 = new Player("Anthony", 0, false);
-        Dice dice1 = new Dice(0);
-        Game game = new Game(player1,player2,dice1);
+        Dice dice = new Dice(0);
+        Game game = new Game(player1,player2,dice);
         game.StartGame();
     }
 }
