29331ee54fe8f5fd3aa7c4f37d03e472088bde2f
antonoeschmidt <s163053@student.dtu.dk>
2019-10-03 10:22:29 +0200

    Lavet ny metode playerTurn(Player player)

--- src/AnthOgAnt/Game.java
+++ src/AnthOgAnt/Game.java
@@ -3,39 +3,47 @@ package AnthOgAnt;
 public class Game {
     private Player player1;
     private Player player2;
-    private Dice dice1;
+    private Dice dice;
 
     public Game(Player player1, Player player2, Dice dice1) {
         this.player1 = player1;
         this.player2 = player2;
-        this.dice1 = dice1;
+        this.dice = dice1;
     }
 
     public void StartGame() {
-        //player1.setPlayerturn(true);
-        int value1;
-        int value2;
         while ((player1.getScore() < 40) && (player2.getScore() < 40)) {
-            value1 = dice1.roll();
-            value2 = dice1.roll();
-            player1.setScore(player1.getScore() + value1);
-            System.out.println("Dice 1 = " + value1);
-            player1.setScore(player1.getScore() + value2);
-            System.out.println("Dice 1 = " + value2);
-            if (value1 == value2) {
-                player1.setScore(0);
-                System.out.println("Too bad, score reset");
-            }
-
-            player2.setScore(player2.getScore() + dice1.roll());
-            System.out.println("Player 1 score :" + player1.getScore());
-            System.out.println("Player 2 score :" + player2.getScore());
+            playerTurn(player1);
+            playerTurn(player2);
+            System.out.println(player1.getName() + " score : " + player1.getScore());
+            System.out.println(player2.getName() + " score :" + player2.getScore());
         }
         if (player1.getScore() > player2.getScore()){
             System.out.println(player1.getName() + " wins with " + player1.getScore() + " points.");
         }
         else {
             System.out.println(player2.getName() + " wins with " + player2.getScore() + " points.");
+        }
+    }
+
+    private void playerTurn(Player player) {
+        int value1;
+        int value2;
+        value1 = dice.roll();
+        value2 = dice.roll();
+        player.setScore(player.getScore() + value1);
+        System.out.println(player.getName() + " slår med første terning = " + value1);
+        player.setScore(player.getScore() + value2);
+        System.out.println(player.getName() + " slår med anden terning = " + value2);
+        if (value1 == 1 && value2 == 1) {
+            player.setScore(0);
+            System.out.println("Ærgerligt, score nulstillet.");
+        } else if (value1 == value2) {
+            value1 = dice.roll();
+            value2 = dice.roll();
+            player.setScore(player.getScore() + value1 + value2);
+            System.out.println("Ekstra slag gav " + value1 +" og " + value2);
+            System.out.println(player.getName() + " har nu " + player.getScore() + " point.");
         }
     }
 
